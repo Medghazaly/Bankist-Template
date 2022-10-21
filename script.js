6081174adc11f1241bcad33bbad44fb63915353a
ghazaly <medghazalyegy@gmail.com>
2022-10-21 11:29:39 +0000

    Edit navLinks Section

--- script.js
+++ script.js
@@ -139,6 +139,7 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   if (e.target.classList.contains("nav__link")) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    navLinks.classList.remove("open");
   }
 });
 
